2ed860d2c7ca15ba8cb109bfe1eb9cb0e2d44a28
sippin <regulationqq@mail.ru>
2025-02-05 14:02:19 +0300

    rounded cards

--- components/FurnitureCard.jsx
+++ components/FurnitureCard.jsx
@@ -9,7 +9,7 @@ export default function FurnitureCard({ id, name, tags, price, image }) {
     return (
         <Link href={`/catalog/${id}`} className='flex flex-col transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300'>
             {/* <Image width={285} height={300} src={image} alt='furniture' /> */}
-            <img width={285} height={300} src={image} alt='furniture' />
+            <img className='rounded-lg' width={285} height={300} src={image} alt='furniture' />
 
             <span className='font-bold text-[#484848] text-[]'>{name}</span>
             <span className=''>{editPrice} ₽</span>
