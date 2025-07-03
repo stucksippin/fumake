4efd9f466cb8c12546f18c227faaabf2a04b0a34
danivelin <113595467+stucksippin@users.noreply.github.com>
2025-07-03 23:04:30 +0300

    add canvas wrapper

--- app/constructor/page.js
+++ app/constructor/page.js
@@ -1,28 +1,25 @@
-// pages/constructor/page.js
 'use client';
+
 import dynamic from 'next/dynamic';
 import { Experience } from './components/Experience';
 import { Interface } from './components/Interface';
 
-export default function ConstructorPage() {
-    const Canvas = dynamic(() => import('@react-three/fiber').then(m => m.Canvas), {
-        ssr: false
-    });
+const Canvas = dynamic(() => import('./components/CanvasWrapper'), {
+    ssr: false,
+});
 
+export default function ConstructorPage() {
     return (
         <div className="min-h-screen flex">
-            {/* Левая часть — трёхмерка */}
             <div className="flex-1 flex">
                 <Canvas
-                    className="flex-1 h-full"        // <-- здесь растягиваем canvas
+                    className="flex-1 h-full"
                     camera={{ position: [3, 3, -9], fov: 25 }}
                     shadows
                 >
                     <Experience />
                 </Canvas>
             </div>
-
-            {/* Правая панель */}
             <div className="w-80 p-4 overflow-auto bg-white/80 backdrop-blur-sm">
                 <Interface />
             </div>
