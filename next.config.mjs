de9b43d444dceef357eb71993ae02ef2c1784da6
danivelin <113595467+stucksippin@users.noreply.github.com>
2025-07-03 22:59:31 +0300

    otmena configa

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    experimental: {
-        serverActions: true, // если используете server actions
-    },
+
 };
 
-module.exports = nextConfig;
+export default nextConfig;
